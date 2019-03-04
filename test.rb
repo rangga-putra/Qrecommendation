43a528f41504c7fdb4216111eb2698ebbe8795be
rangga-putra <47810907+rangga-putra@users.noreply.github.com>
2019-03-04 11:58:24 +0700

    Delete test.rb

--- test.rb
+++
@@ -1,8 +0,0 @@
-require 'net/http'
-require 'json'
-
-urinilai = URI('http://5c6f9b3369738000148aeab0.mockapi.io/assessment')
-
-resnilai = Net::HTTP.get_response(urinilai)
-
-puts JSON.parse(resnilai.body)
